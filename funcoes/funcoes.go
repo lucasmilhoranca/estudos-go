d072f7778cab8bb050dad8c450e2c8769d5ef10e
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-03-27 13:21:28 -0300

    feat: adicionando arquivo de estudo de funções

---
+++ funcoes/funcoes.go
@@ -0,0 +1,84 @@
+package main
+
+import "fmt"
+
+func funcaoSimples() {
+	fmt.Println("Função simples")
+}
+
+func funcaoParametrizada(nome string) {
+	fmt.Println("Olá,", nome)
+}
+
+func funcaoParametrizadaMesmoTipo(nome, sobrenome string) {
+	fmt.Println("Olá,", nome, sobrenome)
+}
+
+func funcaoRetorno() string {
+	return "Função com retorno"
+}
+
+func funcaoRetornoMultiplosValores() (string, string) {
+	return "Função com retorno", "Multiplos valores"
+}
+
+func funcaoRetornoNomeado() (primeiroRetorno string, segundoRetorno string) {
+	primeiroRetorno = "Retorno 1"
+	segundoRetorno = "Retorno 2"
+	return
+}
+
+func funcaoVariadica(numeros ...int) int {
+	total := 0
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total
+}
+
+func scope() func() int {
+	outerVar := 2
+	foo := func() int {
+		return outerVar
+	}
+	return foo
+}
+
+func outer() (func() int, int) {
+	outerVar := 2
+	inner := func() int {
+		outerVar += 99
+		return outerVar
+	}
+	inner()
+	return inner, outerVar
+}
+
+func main() {
+	//atribuindo uma função a uma variável estilo => no javascript
+	funcao := func(a, b int) int {
+		return a + b
+	}
+	fmt.Println(funcao(1, 2))
+
+	funcaoSimples()
+
+	funcaoParametrizada("Jhon")
+
+	funcaoParametrizadaMesmoTipo("Jhon", "Doe")
+
+	fmt.Println(funcaoRetorno())
+
+	fmt.Println(funcaoRetornoMultiplosValores())
+
+	fmt.Println(funcaoRetornoNomeado())
+
+	fmt.Println(funcaoVariadica(1, 2, 3, 4, 5))
+
+	foo := scope()
+	fmt.Println(foo())
+
+	inner, outer := outer()
+	fmt.Println(inner())
+	fmt.Println(outer)
+}
