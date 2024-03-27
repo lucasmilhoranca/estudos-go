dea90aba9829c638edc3cf0a006fb58b68c1ae3c
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-03-27 18:33:43 -0300

    refact: adicionando novos casos de if/else

--- if-else/if_else.go
+++ if-else/if_else.go
@@ -3,12 +3,25 @@ package main
 import "fmt"
 
 func main() {
-
+	fmt.Println("If-else")
 	number := 10
 
 	if number > 5 {
-		fmt.Println("7 maior que 5")
+		fmt.Println(number, "maior que 5")
 	} else {
-		fmt.Println("7 menor que 5")
+		fmt.Println(number, "menor que 5")
+	}
+
+	if x := number * number; x < 10 {
+		fmt.Println(x, "menor que 10")
+	} else {
+		fmt.Println(x, "maior que 10")
+	}
+
+	var val interface{}
+	val = "foo"
+	if str, ok := val.(string); ok {
+
+		fmt.Println(str, ok)
 	}
 }
