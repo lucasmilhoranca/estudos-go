a14fa6d1d0de89b385d6582cebee50151b96080f
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-04-10 00:13:33 -0300

    refact: adicionando novos exemplos no arquivo de estudo de structs

--- structs/structs.go
+++ structs/structs.go
@@ -1,9 +1,26 @@
 package main
 
+import "fmt"
+
 type Vertex struct {
 	X, Y int
 }
 
+func (v Vertex) Abs() int {
+	return v.X*v.X + v.Y*v.Y
+}
+
+func (v *Vertex) add(n int) {
+	v.X += n
+	v.Y += n
+}
+
 func main() {
-	
+	var v = Vertex{1, 2}
+	fmt.Println(v)
+	fmt.Println(v.X, v.Y)
+	fmt.Println(v.Abs())
+
+	v.add(3)
+	fmt.Println(v)
 }
