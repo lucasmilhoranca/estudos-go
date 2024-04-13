29ca892c490375b4372b41ee015ede7f4277a7d6
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-04-13 16:04:53 -0300

    feat: adicionando estudos sobre composição de struct

--- structs/structs.go
+++ structs/structs.go
@@ -6,6 +6,23 @@ type Vertex struct {
 	X, Y int
 }
 
+// Composição de structs
+type Pessoa struct {
+	name string
+	age  int
+	Enderaco
+}
+
+type Enderaco struct {
+	city  string
+	state string
+}
+
+//sem * troca apenas dentro da função
+func (p *Pessoa) trocarNome(newName string) {
+	p.name = newName
+}
+
 func (v Vertex) Abs() int {
 	return v.X*v.X + v.Y*v.Y
 }
@@ -20,6 +37,18 @@ func main() {
 	fmt.Println(v)
 	fmt.Println(v.X, v.Y)
 	fmt.Println(v.Abs())
+
+	pessoa := Pessoa{
+		name: "Lucas",
+		age: 20,
+	}
+
+	pessoa.Enderaco.city = "São Paulo"
+	pessoa.state = "SP"
+	fmt.Println(pessoa)
+
+	pessoa.trocarNome("João")
+	fmt.Println(pessoa)
 
 	v.add(3)
 	fmt.Println(v)
